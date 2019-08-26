0277e4840d6c0f3249d8154ca02d24ad40c4e496
apiloqbc <48203828+apiloqbc@users.noreply.github.com>
2019-08-26 11:55:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,39 +2,14 @@
 
 set -e
 
-FOLDER=$1
-GITHUB_USERNAME=$2
-STARTER_NAME="${3:-'name'}"
-BASE=$(pwd)
+git config --global user.email "myemail@email.com"
+git config --global user.name "$GITHUB_ORG"
 
-git config --global user.email "johno-actions-push-subdirectories@example.org"
-git config --global user.name "$GITHUB_USERNAME"
+echo "Upgrading version"
 
-echo "Cloning folders in $FOLDER and pushing to $GITHUB_USERNAME"
-
-# sync to read-only clones
-for folder in $FOLDER/*; do
-  [ -d "$folder" ] || continue # only directories
-  cd $BASE
-
-  echo "Pushing $folder"
-
-  NAME=$(cat $folder/package.json | jq --arg name "$STARTER_NAME" -r '.[$name]')
-  CLONE_DIR="__${NAME}__clone__"
-
-  # clone, delete files in the clone, and copy (new) files over
-  # this handles file deletions, additions, and changes seamlessly
-  git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$NAME.git $CLONE_DIR
-  cd $CLONE_DIR
-  find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions in monorepo)
-  cp -r $BASE/$folder/. .
-
-  rm -rf yarn.lock
-  yarn
-
-  git add .
-  git commit --message "Update $NAME from $GITHUB_REPOSITORY"
-  git push origin master
-
-  cd $BASE
-done
+git clone --depth 1 https://$API_TOKEN_GITHUB@github.com/$GITHUB_ORG/$REPO_NAME.git $CLONE_DIR
+cd $CLONE_DIR
+date >> README.md
+git add .
+git commit --message "Update $NAME from $GITHUB_REPOSITORY"
+git push origin master
